b0c70eaff2efaa2e4b5bf0c6d88756528d76ac7e
Anindya Saha <anindya_2906@yahoo.co.in>
2021-01-15 22:40:16 +0530

    created storage class skeleton

---
+++ dqn/storage.py
@@ -0,0 +1,16 @@
+from collections import deque, namedtuple
+
+
+class ReplayBuffer():
+
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def sample(self):
+        pass
+
+    def __len__(self):
+        pass
